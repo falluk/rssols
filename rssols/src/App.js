f34a9ae2a83be47d7c9b909719b8166857563c15
Roni Serra <40413583+falluk@users.noreply.github.com>
2020-12-03 10:49:18 -0300

    Primeira alteração

---
+++ rssols/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Página React em contrução! 
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/roni-serra-009a9751/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Saiba mais sobre mim.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
